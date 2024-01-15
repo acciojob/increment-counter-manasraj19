8e1f3307e0cefba315f5b1595d4085cdbfbc4deb
Manas Raj <119037651+manasraj19@users.noreply.github.com>
2024-01-15 18:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let counterElem = document.getElementById("counter");
+let IncrementButton = document.getElementById("incrementBtn");
+
+let counterValue = 0;
+
+function incrementCounter(){
+	alert("un-incremented value" + counterValue);
+	counterValue++;
+	counterElem.textContent = counterValue;
+	
+}
+IncrementButton.addEventListener("click", incrementCounter);
